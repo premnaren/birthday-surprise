f44f3454c4101e136ee1d01714b0fe5a6b418853
Prem Naren <prem@example.com>
2026-02-02 20:05:13 +0530

    q1

--- client/src/App.js
+++ client/src/App.js
@@ -38,7 +38,7 @@ const quests = [
       question: "Let's go back to the start. This is something we completed together for the very first time. You started it, and I finished it, sparking the first words between us.", 
       answer: "butterfly", 
       hint: "Check your memory of your notebook of first sem.",
-      reward: "come again on 7PM in the evening😁😁",
+      reward: "come again on 4PM in the evening😁😁",
     },
     { 
       id: 5, 
